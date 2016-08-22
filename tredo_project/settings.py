ab031eab9cd2a9d5d7873c5b0ac2be3b33857689
Marek Lapinski <mrk.lapinski@gmail.com>
2016-08-22 13:03:36 +0200

    testing gitignore

--- tredo_project/settings.py
+++ tredo_project/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tredo',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,3 +82,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = ''
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    #'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+#Templates
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
